b744ed933969c46b6261ae051a6e0dd94d04b062
Sajid <sbicoredev@gmail.com>
2026-07-04 21:44:27 +0600

    refactor: update landing page ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,97 @@
+import Image from "next/image";
+import Link from "next/link";
+
 import { Footer } from "~/components/sections/footer";
 import { Header } from "~/components/sections/header";
-import { HeroSection } from "~/components/sections/hero-section";
-import { IntegrationsSection } from "~/components/sections/integrations-section";
-import { PricingSection } from "~/components/sections/pricing-section";
-import { StatsSection } from "~/components/sections/stats-section";
-import { TestimonialSection } from "~/components/sections/testimonial-section";
+import { Button } from "~/components/ui/button";
+import { siteConfig } from "~/configs/site-config";
+import { AUTH_URI } from "~/constants/auth";
+import { cn } from "~/lib/utils";
+import { checkAuth } from "~/services/auth";
 
-export default function HomePage() {
+export default async function HomePage() {
+  const auth = await checkAuth();
   return (
-    <>
+    <div className="relative flex h-full min-h-screen w-full flex-col bg-card">
       <Header />
-      <main>
-        <HeroSection />
-        <IntegrationsSection />
-        <StatsSection />
-        <TestimonialSection />
-        <PricingSection />
-      </main>
+      <div className="z-10 mx-auto flex w-full max-w-5xl flex-col gap-4 px-6">
+        <div className="z-10 flex h-full w-full flex-col items-center justify-center gap-4 p-12 md:p-24">
+          <Button
+            className={cn(
+              "hidden h-8 rounded-full bg-white/40 px-3 font-bold text-sm backdrop-blur hover:text-primary md:flex dark:bg-secondary"
+            )}
+            variant="outline"
+          >
+            <span className="flex items-center font-medium text-primary/60">
+              Introducing
+              <svg
+                className="mx-1 h-3.5 w-3.5 text-primary"
+                strokeLinejoin="round"
+                viewBox="0 0 16 16"
+              >
+                <path
+                  clipRule="evenodd"
+                  d="M0.5 0.5H5.75L9.48421 5.71053L14 0.5H16L10.3895 6.97368L16.5 15.5H11.25L7.51579 10.2895L3 15.5H1L6.61053 9.02632L0.5 0.5ZM12.0204 14L3.42043 2H4.97957L13.5796 14H12.0204Z"
+                  fill="currentColor"
+                  fillRule="evenodd"
+                />
+              </svg>
+            </span>
+            {siteConfig.name}
+          </Button>
+          <h1 className="text-center font-bold text-6xl text-primary leading-tight md:text-7xl lg:leading-tight">
+            Production Ready
+            <br />
+            SaaS Stack for Nextjs
+          </h1>
+          <p className="max-w-3xl text-center text-lg text-muted-foreground leading-normal md:text-xl">
+            Launch in hours with a modern{" "}
+            <span className="font-medium text-primary">
+              Production-Ready Stack
+            </span>
+            <br className="hidden lg:inline-block" /> Stripe integration.
+            Next.js-powered. Open Source.
+          </p>
+          <div className="mt-2 flex w-full items-center justify-center gap-2">
+            <Button
+              className="hidden sm:flex"
+              nativeButton={false}
+              render={
+                <Link href={auth?.user ? "/dashboard" : AUTH_URI.signin} />
+              }
+              size="sm"
+            >
+              {auth?.user ? "Go To Dashboard" : "Get Started"}
+            </Button>
+            <Button
+              className="hidden sm:flex dark:bg-secondary dark:hover:opacity-80"
+              nativeButton={false}
+              render={
+                <Link
+                  href={siteConfig.githubRepo}
+                  rel="noreferrer"
+                  target="_blank"
+                />
+              }
+              size="sm"
+              variant="outline"
+            >
+              Explore Documentation
+            </Button>
+          </div>
+        </div>
+      </div>
       <Footer />
-    </>
+
+      <Image
+        alt="Hero"
+        className="fixed top-0 left-0 z-0 h-full w-full opacity-60 dark:invert"
+        height={500}
+        src="/assets/shadow.png"
+        width={500}
+      />
+      <div className="base-grid fixed h-screen w-screen opacity-40" />
+      <div className="fixed bottom-0 h-screen w-screen bg-linear-to-t from-[hsl(var(--card))] to-transparent" />
+    </div>
   );
 }
--- src/components/sections/header.tsx
+++ src/components/sections/header.tsx
@@ -1,109 +1,171 @@
 "use client";
 
-import { MenuIcon, XIcon } from "lucide-react";
+import {
+  BellIcon,
+  CreditCardIcon,
+  LogOutIcon,
+  StarIcon,
+  UserIcon,
+} from "lucide-react";
 import Link from "next/link";
-import React from "react";
+import { useRouter } from "next/navigation";
 
 import { Logo } from "~/components/logo";
+import { ThemeToggle } from "~/components/theme-toggle";
 import { Button } from "~/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "~/components/ui/dropdown-menu";
+import { UserAvatar } from "~/components/user-avatar";
+import { siteConfig } from "~/configs/site-config";
 import { AUTH_URI } from "~/constants/auth";
 import { useAuth } from "~/contexts/auth-context";
-import { cn } from "~/lib/utils";
-
-import { LocaleSwitcher } from "../locale-switcher";
-import { ThemeToggle } from "../theme-toggle";
-import { Nav } from "./nav";
+import { authClient } from "~/services/auth/auth-client";
 
 export const Header = () => {
+  const router = useRouter();
   const { user } = useAuth();
-  const [menuState, setMenuState] = React.useState(false);
-  const [isScrolled, setIsScrolled] = React.useState(false);
 
-  React.useEffect(() => {
-    const handleScroll = () => {
-      setIsScrolled(window.scrollY > 50);
-    };
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, []);
+  const handleSignout = async () => {
+    await authClient.signOut({
+      fetchOptions: {
+        onSuccess: () => {
+          router.push(AUTH_URI.signin);
+          router.refresh();
+        },
+      },
+    });
+  };
 
   return (
     <header>
-      <div
-        className="fixed z-50 w-full px-2"
-        data-state={menuState && "active"}
-      >
-        <div
-          className={cn(
-            "mx-auto mt-2 max-w-6xl px-6 transition-all duration-300 lg:px-12",
-            isScrolled &&
-              "max-w-4xl rounded-2xl border bg-background/50 backdrop-blur-lg lg:px-5"
-          )}
-        >
-          <div className="relative flex flex-wrap items-center justify-between gap-6 py-3 lg:gap-0 lg:py-4">
-            <div className="flex w-full justify-between lg:w-auto">
-              <Link
-                aria-label="home"
-                className="flex items-center space-x-2"
-                href="/"
+      <div className="sticky top-0 z-50 mx-auto flex w-full max-w-5xl items-center justify-between p-6 py-3">
+        <Link className="flex h-10 items-center gap-1" href="/">
+          <Logo />
+        </Link>
+        <div className="flex items-center gap-4">
+          <div className="flex items-center gap-6">
+            <Button
+              className="group flex gap-3 px-0 text-primary/80 hover:text-primary hover:no-underline"
+              nativeButton={false}
+              render={
+                <Link
+                  href={siteConfig.githubRepo}
+                  rel="noreferrer"
+                  target="_blank"
+                />
+              }
+              size="sm"
+              variant="link"
+            >
+              <svg
+                className="size-6"
+                fill="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
               >
-                <Logo />
-              </Link>
-
-              <div className="flex gap-3">
-                <div className="flex gap-2 lg:hidden">
-                  <LocaleSwitcher size="icon-sm" variant="outline" />
-                  <ThemeToggle size="icon-sm" variant="outline" />
-                </div>
-
-                <button
-                  aria-label={menuState === true ? "Close Menu" : "Open Menu"}
-                  className="relative z-20 -m-2.5 -mr-4 block cursor-pointer p-2.5 lg:hidden"
-                  onClick={() => setMenuState(!menuState)}
-                  type="button"
-                >
-                  <MenuIcon className="m-auto size-6 in-data-[state=active]:rotate-180 in-data-[state=active]:scale-0 in-data-[state=active]:opacity-0 duration-200" />
-                  <XIcon className="absolute inset-0 m-auto size-6 -rotate-180 in-data-[state=active]:rotate-0 in-data-[state=active]:scale-100 scale-0 in-data-[state=active]:opacity-100 opacity-0 duration-200" />
-                </button>
-              </div>
-            </div>
-
-            <Nav />
-
-            <div className="hidden gap-2 lg:flex">
-              <LocaleSwitcher size="icon-sm" variant="outline" />
-              <ThemeToggle size="icon-sm" variant="outline" />
-              {user ? (
-                <Button
-                  nativeButton={false}
-                  render={<Link href="/dashboard" />}
-                  size="sm"
-                  variant="outline"
-                >
-                  Dashbaord
-                </Button>
-              ) : (
-                <>
-                  <Button
-                    className={cn(isScrolled && "lg:hidden")}
-                    nativeButton={false}
-                    render={<Link href={AUTH_URI.signin} />}
-                    size="sm"
-                    variant="outline"
-                  >
-                    Login
-                  </Button>
-                  <Button
-                    nativeButton={false}
-                    render={<Link href={AUTH_URI.signup} />}
-                    size="sm"
-                  >
-                    {isScrolled ? "Get Started" : "Signup"}
-                  </Button>
-                </>
-              )}
-            </div>
+                <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
+              </svg>
+              <span className="hidden select-none items-center gap-1 rounded-full bg-green-500/5 px-2 py-1 pr-2.5 font-medium text-green-600 text-xs tracking-tight ring-1 ring-green-600/20 ring-inset backdrop-blur-sm md:flex dark:bg-yellow-800/40 dark:text-yellow-100 dark:ring-yellow-200/50">
+                <StarIcon
+                  className="h-3 w-3 text-green-600 dark:text-yellow-100"
+                  fill="currentColor"
+                />
+                Star on GitHub
+              </span>
+            </Button>
           </div>
+          <Button
+            nativeButton={false}
+            render={
+              <Link
+                href={siteConfig.author.x}
+                rel="noreferrer"
+                target="_blank"
+              />
+            }
+            size="icon-sm"
+            variant="link"
+          >
+            <svg
+              className="size-4 text-primary"
+              strokeLinejoin="round"
+              viewBox="0 0 16 16"
+            >
+              <path
+                clipRule="evenodd"
+                d="M0.5 0.5H5.75L9.48421 5.71053L14 0.5H16L10.3895 6.97368L16.5 15.5H11.25L7.51579 10.2895L3 15.5H1L6.61053 9.02632L0.5 0.5ZM12.0204 14L3.42043 2H4.97957L13.5796 14H12.0204Z"
+                fill="currentColor"
+                fillRule="evenodd"
+              />
+            </svg>
+          </Button>
+          <ThemeToggle size="icon-sm" variant="link" />
+          <Button
+            nativeButton={false}
+            render={<Link href={user ? "/dashboard" : AUTH_URI.signin} />}
+            size="sm"
+          >
+            {user ? "Dashboard" : "Get Started"}
+          </Button>
+          {user && (
+            <DropdownMenu>
+              <DropdownMenuTrigger>
+                <UserAvatar alt={user.name} src={user.image} />
+              </DropdownMenuTrigger>
+              <DropdownMenuContent
+                align="end"
+                className="min-w-56 rounded-lg"
+                side="bottom"
+                sideOffset={4}
+              >
+                <DropdownMenuGroup>
+                  <DropdownMenuLabel className="p-0 font-normal">
+                    <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
+                      <UserAvatar alt={user.name} src={user?.image || null} />
+                      <div className="grid flex-1 text-left text-sm leading-tight">
+                        <span className="truncate font-medium">
+                          {user.name}
+                        </span>
+                        <span className="truncate text-muted-foreground text-xs">
+                          {user.email}
+                        </span>
+                      </div>
+                    </div>
+                  </DropdownMenuLabel>
+                </DropdownMenuGroup>
+                <DropdownMenuSeparator />
+                <DropdownMenuGroup>
+                  <DropdownMenuItem>
+                    <UserIcon />
+                    Account
+                  </DropdownMenuItem>
+                  <DropdownMenuItem>
+                    <CreditCardIcon />
+                    Billing
+                  </DropdownMenuItem>
+                  <DropdownMenuItem
+                    nativeButton={false}
+                    render={<Link href="/dashboard/notifications" />}
+                  >
+                    <BellIcon />
+                    Notifications
+                  </DropdownMenuItem>
+                </DropdownMenuGroup>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem onClick={handleSignout}>
+                  <LogOutIcon />
+                  Log out
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          )}
         </div>
       </div>
     </header>
